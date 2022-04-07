1ddb58364fe0cd5ad45debb47c280ad92bc5db25
Shomatas <76132980+Shomatas@users.noreply.github.com>
2022-04-07 05:49:26 +0300

    second-comit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import reportWebVitals from './reportWebVitals';
 import {store} from './store/createStore.js';
 import render from "./render.js";
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
-render(store);
+function timeout(delay: number) {
+    return new Promise( res => setTimeout(res, delay) );
+}
+
+setTimeout(function () {
+  render(store);
+}, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
